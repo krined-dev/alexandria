b0ef2667c57d497f5412c86e4f9dda0a50017f6c
Kristian Nedrevold <kne009@uit.no>
2023-03-06 11:31:23 +0100

    BAC-37 Updated the r2handler.kt to be quite a bit more ergonomic. Will add some documentation to it later.

--- alexandria/src/main/kotlin/no/hnikt/db/handler/r2handler.kt
+++ alexandria/src/main/kotlin/no/hnikt/db/handler/r2handler.kt
@@ -1,8 +1,12 @@
 package no.hnikt.db.handler
 
 import arrow.core.continuations.Effect
+import arrow.core.continuations.EffectScope
 import arrow.core.continuations.effect
+import arrow.core.nonFatalOrThrow
+import arrow.core.valid
 import io.r2dbc.spi.*
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirst
@@ -17,57 +21,83 @@ typealias R2DBCMapper<T> = (row: Row, rowMetadata: RowMetadata) -> T
 
 sealed interface R2DBCError {
     object Error: R2DBCError
-    object TransactionError : R2DBCError
+    data class TransactionError(val stacktrace: String) : R2DBCError
 }
 
 object R2DBC {
     inline fun <E, T>ConnectionFactory.transaction(
-        failWith: E,
-        crossinline handler: context(Connection) () -> Effect<R2DBCError, T>): Effect<E, T> = effect {
+        crossinline handler: ConnectionHandle<E>.() -> Effect<E, T>): Effect<E, T> = effect {
         val connection = create().awaitFirst()
         try {
-            val value = handler(connection)
+            val value = handler(ConnectionHandle(connection, this))
             connection.commitTransaction()
             value.fold({
                 connection.rollbackTransaction()
-                shift(failWith)
+                shift(it)
             }, { it })
         } catch (e: Exception) {
             connection.rollbackTransaction()
-            if (e is kotlinx.coroutines.CancellationException) {
-                throw e
-            }
-            shift(failWith)
+            throw e
         }
     }
+
 }
 
-
-/**
- * [execute] executes an insert/update/delete and returns the amount of affected rows
- */
-context (Connection)
-fun execute(sql: String, vararg params: Any) = effect<R2DBCError, Long> {
-    val stmt = createStatement(sql)
-    params.forEachIndexed { idx, param ->
-        stmt.bind(idx, param)
+class ConnectionHandle<E>(connection: Connection, raise: EffectScope<E>): Connection by connection, EffectScope<E> by raise {
+    /**
+     * [select] Executes a select statement and maps the result into a Flow<T> with a given mapper
+     */
+    inline fun <T: Any>select(
+        noinline r2dbcMapper: R2DBCMapper<T>,
+        sql: String,
+        vararg params: Any,
+        crossinline failWith: (Exception) -> E,
+    ) = effect<E, Flow<T>> {
+        try {
+            val stmt = createStatement(sql)
+            params.forEachIndexed { idx, param ->
+                stmt.bind(idx, param)
+            }
+            stmt.add().execute().awaitFirst()
+                .map(r2dbcMapper)
+                .asFlow()
+        } catch (e: Exception) {
+            e.nonFatalOrThrow()
+            shift(failWith(e))
+        }
     }
 
-    stmt.add().execute().awaitFirst()
-        .rowsUpdated
-        .awaitFirst()
+    /**
+     * [execute] executes an insert/update/delete and returns the amount of affected rows
+     */
+    inline fun <E>execute(
+        sql: String,
+        vararg params: Any,
+        crossinline failWith: (Exception) -> E,
+    ) = effect<E, Long> {
+        try {
+
+            val stmt = createStatement(sql)
+            params.forEachIndexed { idx, param ->
+                stmt.bind(idx, param)
+            }
+
+            stmt.add().execute().awaitFirst()
+                .rowsUpdated
+                .awaitFirst()
+        } catch (e: Exception) {
+            e.nonFatalOrThrow()
+            shift(failWith(e))
+        }
+    }
+
+    inline fun executeR2DBC(sql: String) = execute<R2DBCError>(sql) {
+        (it as R2dbcException).toTypedError()
+    }
 }
 
-/**
- * [select] Executes a select statement and maps the result into a Flow<T> with a given mapper
- */
-context (Connection)
-inline fun <reified T: Any>select(sql: String,  noinline r2dbcMapper: R2DBCMapper<T>, vararg params: Any) = effect<R2DBCError, Flow<T>> {
-    val stmt = createStatement(sql)
-
-    params.forEachIndexed { idx, param ->
-        stmt.bind(idx, param)
-    }
-
-    stmt.add().execute().awaitFirst().map(r2dbcMapper).asFlow()
+// TODO add some proper mapping here
+fun R2dbcException.toTypedError(): R2DBCError = when (this) {
+        is R2dbcException -> R2DBCError.TransactionError(this.message ?: "Unexpected error")
+        else -> R2DBCError.Error
 }
--- alexandria/src/main/kotlin/no/hnikt/repository/File repository.kt
+++ alexandria/src/main/kotlin/no/hnikt/repository/File repository.kt
@@ -4,13 +4,8 @@ import arrow.core.continuations.Effect
 import arrow.core.continuations.EffectScope
 import arrow.core.continuations.effect
 import io.r2dbc.spi.ConnectionFactory
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.single
-import kotlinx.coroutines.flow.take
-import no.hnikt.db.handler.R2DBC
-import no.hnikt.db.handler.R2DBCMapper
-import no.hnikt.db.handler.execute
-import no.hnikt.db.handler.select
+import no.hnikt.db.handler.*
 import no.hnikt.domain.models.files.StoredFile
 import no.hnikt.errors.FileRepositoryError
 
@@ -24,31 +19,36 @@ interface FileRepository {
     fun updateFile(file: StoredFile): Effect<FileRepositoryError, StoredFile>
 }
 
-context(R2DBC)
+context(EffectScope<FileRepositoryError>, R2DBC)
 fun fileRepository(conn: ConnectionFactory) = object : FileRepository {
     override fun storeFile(file: StoredFile) = effect {
-        val inserted = conn.transaction(FileRepositoryError.InsertError("Error inserting file: ${file.filename}")) {
-            execute(
+        val inserted = conn.transaction<FileRepositoryError, Long> { execute(
                 """
                 insert into files (filename, version, bytes, registry) values (?, ?, ?)
-                """.trimIndent(), file.filename, file.version, file.bytes, file.registry)
+                """.trimIndent(), file.filename, file.version, file.bytes, file.registry) {
+                FileRepositoryError.InsertError("Insert failed with exception: ${it.stackTraceToString()}")
+            }
         }.bind()
         ensure(inserted == 1L) { FileRepositoryError.InsertError("Inserted wrong amount of files: $inserted")}
         selectFile(file.filename, file.registry).bind()
     }
 
     override fun selectFile(filename: String, registry: String): Effect<FileRepositoryError, StoredFile> = effect {
-        conn.transaction(FileRepositoryError.FileNotFound(filename)) {
-            select(
+        conn.transaction {
+            select(storedFileMapper,
                 """
-               select * from files where filename = ? and registry = ? 
-            """.trimIndent(), storedFileMapper, filename, registry
-            )
+                select * from files where filename = ? and registry = ? 
+                """.trimIndent(), storedFileMapper, filename, registry
+            ) {
+                FileRepositoryError.FileSelectError(it.stackTraceToString())
+            }
         }.bind().single()
     }
     override fun deleteFile(fileName: String, registry: String): Effect<FileRepositoryError, Long> =
-        conn.transaction(FileRepositoryError.FileNotFound(fileName)) {
-            execute("""delete from files where filename = ? and registry = ?""", fileName, registry)
+        conn.transaction {
+            execute("""delete from files where filename = ? and registry = ?""", fileName, registry) {
+               FileRepositoryError.FileNotFound("File not found: $fileName")
+            }
         }
 
     override fun updateFile(file: StoredFile): Effect<FileRepositoryError, StoredFile> = TODO()
